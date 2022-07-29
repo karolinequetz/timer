9fef9ba6f9b59f8b2cad661a2f58b69106e4a548
karolsouzaq@gmail.com <karolsouzaq@gmail.com>
2022-07-28 21:32:13 -0300

    Criado componente Header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/themes/global';
-export function App() {
-	return (
-		<ThemeProvider theme={defaultTheme}>
-			<h1>Hello Karol</h1>
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/themes/global'
 
-      <GlobalStyle/>
-		</ThemeProvider>
-	);
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
